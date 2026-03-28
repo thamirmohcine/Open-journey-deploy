223a19dc903a8a1023d8f807a46b1caff6d84c2f
thamirmohcine <thamirmohsine@gmail.com>
2026-03-28 22:13:12 +0100

    deploy : 3.21

--- api/index.js
+++ api/index.js
@@ -3,81 +3,89 @@ const morgan = require('morgan')
 const path = require('path')
 const cors = require('cors')
 const app = express()
+require('dotenv').config();
+
+const Person = require('../models/person');
+const { Query } = require('mongoose');
 
 app.use(express.json());
 app.use(cors())
+
 
 morgan.token('body', (req, res) => {
     if (req.method === 'POST') {
         return JSON.stringify(req.body);
     }
+    return ''
 })
+
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+
+
 app.use(express.static(path.join(__dirname, '..', 'dist')))
-let persons = [{
-    "id": "1",
-    "name": "Arto Hellas",
-    "number": "040-123456"
-}, {
-    "id": "2",
-    "name": "Ada Lovelace",
-    "number": "39-44-5323523"
-}, {
-    "id": "3",
-    "name": "Dan Abramov",
-    "number": "12-43-234345"
-}, {
-    "id": "4",
-    "name": "Mary Poppendieck",
-    "number": "39-23-642312200000000000000000000000000000"
-}];
 
-
-app.get('/api/persons', (request, response) => {
-    response.send(persons);
+app.get('/api/persons', (request, response, next) => {
+    Person.find({}).then(persons => {
+        response.json(persons)
+    }).catch(error => next(error))
 });
 
-app.get('/api/persons/:id', (request, response) => {
-    const id = request.params.id;
-    const person = persons.find(p => p.id === id);
-    if (person)
-        return response.send(person);
-    response.status(404).end();
 
-})
+app.post('/api/persons', (request, response, next) => {
 
-app.delete('/api/persons/:id', (request, response) => {
-    const id = request.params.id;
-    persons = persons.filter(p => p.id !== id);
-
-    response.status(204).end();
-})
-
-app.post('/api/persons', (request, response) => {
-
-    if (request.body.name === undefined || request.body.number === undefined) {
+    if (!request.body.name || !request.body.number) {
         return response.status(400).send({
             error: "name or number Missing !"
         });
     }
+    const person = new Person({
+        name: request.body.name,
+        number: request.body.number
+    });
+    person.save().then(savedPerson => {
+        response.status(201).json(savedPerson)
+    }).catch(error => next(error))
+})
 
-    const nameExists = persons.find(p => p.name === request.body.name);
-    if (nameExists) {
-        return response.status(400).send({ error: "name must be unique" })
-    }
+app.delete('/api/persons/:id', (request, response, next) => {
+    Person.findByIdAndDelete(request.params.id)
+    .then(result => {
+        response.status(204).end();
+    })
+    .catch(error => next(error))
+})
+
+app.put('/api/persons/:id', (request, response, next) => {
     const person = {
-        id: Math.floor(Math.random() * 1000000).toString(),
         name: request.body.name,
         number: request.body.number
     }
-    persons = persons.concat(person);
-    response.status(201).json(person);
+
+    Person.findByIdAndUpdate(request.params.id, person, {new: true, runValidators: true, context: 'query'})
+    .then(UpdatedPerson => {
+        if (UpdatedPerson){
+            response.json(UpdatedPerson)
+        }else {
+            response.status(404).end()
+        }
+    })
+    .catch(error => next(error))
 })
 
-app.get(/^\/.*$/, (req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'dist', 'index.html'))
-})
+const ErrorHandler = (error, request, response, next) => {
+    console.log(error.message)
+
+    if (error.name === 'CastError'){
+        return response.status(400).send({error: 'malformed id'})
+    }
+    if (error.name === 'ValidationError')
+        return response.status(400).send({error: error.message})
+
+    next(error);
+}
+
+app.use(ErrorHandler)
 
 if (process.env.NODE_ENV !== 'test') {
     const PORT = process.env.PORT || 3001
