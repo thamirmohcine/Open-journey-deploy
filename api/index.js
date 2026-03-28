5084882c2c128619224ba6bd8cc1e779219840ff
thamirmohcine <thamirmohsine@gmail.com>
2026-03-28 22:47:05 +0100

    fix database connection

--- api/index.js
+++ api/index.js
@@ -23,7 +23,7 @@ morgan.token('body', (req, res) => {
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 
-app.use(express.static(path.join(__dirname, '..', 'dist')))
+app.use(express.static('dist'))
 
 app.get('/api/persons', (request, response, next) => {
     Person.find({}).then(persons => {
